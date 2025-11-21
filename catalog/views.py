adad04513f2d4a61a3529daf1fe83900a63250ad
Aleksandr Komoplev <konoplev.a.a0000@gmail.com>
2025-11-22 00:04:12 +1000

    commit 4

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def catalog(request):
-    return render(request, 'catalog.html')
+def home(request):
+    return render(request, 'home.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
