35c598cd164f2c7286221552c2e01a9c10cd83bf
Aleksandr Komoplev <konoplev.a.a0000@gmail.com>
2025-12-04 16:18:12 +1000

    completed 4 tasks, fix models

--- catalog/models.py
+++ catalog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Наименование категории')
-    description = models.TextField(verbose_name='Описание категории')
+    name = models.CharField(max_length=150, verbose_name='Наименование категории', help_text='Введите категорию')
+    description = models.TextField(verbose_name='Описание категории', help_text='Введите категорию')
 
     class Meta:
         verbose_name = 'категория'
@@ -12,10 +12,10 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Наименование продукта')
-    description = models.TextField(verbose_name='Описание продукта')
-    image = models.ImageField(upload_to='products/', verbose_name='Изображение продукта')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=150, verbose_name='Наименование продукта', help_text='Введите наименование')
+    description = models.TextField(verbose_name='Описание продукта', help_text='Введите Описание')
+    image = models.ImageField(upload_to='products/',blank=True, null=True, verbose_name='Изображение продукта')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена продукта')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания продукта')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования продукта')
