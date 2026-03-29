ddc06f47a50a98bc0e22856a952015f21848f960
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-03-29 21:07:53 +1000

    flake8

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,7 @@ LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'catalog:product_list'
 LOGOUT_REDIRECT_URL = 'catalog:product_list'
 
-#Настройки почтового сервиса
+# Настройки почтового сервиса
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
@@ -142,7 +142,7 @@ EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-SERVER_EMAIL =  EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Кэширование
